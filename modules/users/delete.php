a3f22abc204191b47911e9548e19a13a676758d3
hieule210924tb <manu210924@gmail.com>
2026-04-14 22:44:08 +0700

    Code tính năng xóa tài khoản người dùng

--- modules/users/delete.php
+++ modules/users/delete.php
@@ -2,3 +2,28 @@
 if (!defined('_HIEU')) {
     die('Truy cập không hợp lệ');
 }
+$getData = filterData('GET'); // lấy id từ url 
+if (!empty($getData['id'])) { // kiểm tra xem id đó có dữ liệu ko
+    $userId = $getData['id'];
+    $checkUser = getRows("SELECT * from users where id ='$userId'");
+    if ($checkUser > 0) {
+        //Xóa tài khoản
+        $checkToken = getRows("SELECT * from token_login where user_id = '$userId'");
+        if ($checkUser > 0) { // Kiểm tra xem tài khoản đó có tồn tại trong token_login ko
+            delete('token_login', "user_id ='$userId'");
+        }
+        $checkDelete = delete('users', "id='$userId'");
+        if ($checkDelete) {
+            setSessionFlash('msg', 'Bạn đã xóa tài khoản thành công.');
+            setSessionFlash('msg_type', 'success');
+            redirect('?module=users&action=list');
+        } else {
+            setSessionFlash('msg', 'Xóa người dùng thất bại.');
+            setSessionFlash('msg_type', 'danger');
+        }
+    } else {
+        setSessionFlash('msg', 'Người dùng không tồn tại.');
+        setSessionFlash('msg_type', 'danger');
+        redirect('?module=users&action=list');
+    }
+}
